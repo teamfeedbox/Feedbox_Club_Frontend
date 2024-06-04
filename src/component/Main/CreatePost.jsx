cedf8945cc4f8a349df51019920ffc5859b8e020
teamfeedbox <120553699+teamfeedbox@users.noreply.github.com>
2024-06-04 18:01:41 +0530

    Update CreatePost.jsx

--- src/component/Main/CreatePost.jsx
+++ src/component/Main/CreatePost.jsx
@@ -142,7 +142,7 @@ const CreatePost = (props) => {
       }
     }
 
-    const data = fetch("http://localhost:8000/create-post", {
+    const data = fetch("https://club-community-feedbox2-0-sdcn-f4nbfkrt9-feedboxs-projects.vercel.app/create-post", {
       method: "post",
       headers: {
         Authorization: "Bearer " + localStorage.getItem("jwt"),
