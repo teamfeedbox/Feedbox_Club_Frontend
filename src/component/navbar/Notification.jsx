874afb5725035f12e7919807dc7aba34d7cac9cc
teamfeedbox <120553699+teamfeedbox@users.noreply.github.com>
2024-06-04 18:17:32 +0530

    Update Notification.jsx

--- src/component/navbar/Notification.jsx
+++ src/component/navbar/Notification.jsx
@@ -37,7 +37,7 @@ const Notification = (props) => {
 
 
   const getData = async () => {
-    let notifi = await fetch(`http://localhost:8000/getNotifications`, {
+    let notifi = await fetch(`https://club-community-feedbox2-0-sdcn-f4nbfkrt9-feedboxs-projects.vercel.app/getNotifications`, {
       headers: {
         Authorization: "Bearer " + localStorage.getItem("jwt"),
       },
